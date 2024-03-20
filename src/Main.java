2afa7154a57ed03b26de767de60e28f137f77afe
Natalia Grishechko <existencia84@gmail.com>
2024-03-20 20:45:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        MyClass.printThreeWords();
+        MyClass.checkSumSign();
+        MyClass.printColor();
+    }
+}
+
+class MyClass {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5; // Задайте любые значения для переменных a и b
+        int b = -3;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 50; // Задайте любое значение для переменной value
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+}
